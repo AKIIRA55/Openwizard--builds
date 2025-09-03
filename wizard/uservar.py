b23fd0eea7124658203e1f1febbf44d8937aab37
AKIIRA55 <jose709francisco@gmail.com>
2025-09-03 18:29:53 -0300

    Estrutura inicial do repositório OpenWizard Build

---
+++ wizard/uservar.py
@@ -0,0 +1,16 @@
+#########################################################
+#                OpenWizard User Settings               #
+#########################################################
+
+WIZARDFILE     = 'openwizard-repo'
+WIZARDNAME     = 'Meu Wizard'
+
+WIZARDFILE_URL = 'https://raw.githubusercontent.com/SEU_USUARIO/openwizard-repo/main/wizard/builds.txt'
+ADDONFILE      = 'https://raw.githubusercontent.com/SEU_USUARIO/openwizard-repo/main/wizard/addons.txt'
+ADVANCEDFILE   = 'https://raw.githubusercontent.com/SEU_USUARIO/openwizard-repo/main/wizard/advancedsettings.txt'
+
+WIZARD_ICON    = 'https://raw.githubusercontent.com/SEU_USUARIO/openwizard-repo/main/wizard/icon.png'
+WIZARD_FANART  = 'https://raw.githubusercontent.com/SEU_USUARIO/openwizard-repo/main/wizard/fanart.jpg'
+
+SHOWMAINT      = 'true'
+AUTOCLEANUP    = 'true'
